d828da67f187016303f5a30d7dd7db50b9e20eb1
galla-placidia <marlis.heck@web.de>
2016-08-27 14:44:09 +0200

    lang/de_DE.php
    add German to your files

---
+++ lang/de_DE.php
@@ -0,0 +1,28 @@
+<?php
+
+global $lang;
+
+$lang['de_DE']['MathSpamProtection']['ZERO'] = 			'null';
+$lang['de_DE']['MathSpamProtection']['ONE'] = 			'eins';
+$lang['de_DE']['MathSpamProtection']['TWO'] = 			'zwei';
+$lang['de_DE']['MathSpamProtection']['THREE'] = 		'drei';
+$lang['de_DE']['MathSpamProtection']['FOUR'] = 			'vier';
+$lang['de_DE']['MathSpamProtection']['FIVE'] = 			'fünf';
+$lang['de_DE']['MathSpamProtection']['SIX'] = 			'sechs';
+$lang['de_DE']['MathSpamProtection']['SEVEN'] = 		'sieben';
+$lang['de_DE']['MathSpamProtection']['EIGHT'] = 		'acht';
+$lang['de_DE']['MathSpamProtection']['NINE'] = 			'neun';
+$lang['de_DE']['MathSpamProtection']['TEN'] = 			'zehn';
+$lang['de_DE']['MathSpamProtection']['ELEVEN'] = 		'elf';
+$lang['de_DE']['MathSpamProtection']['TWELVE'] = 		'zwölf';
+$lang['de_DE']['MathSpamProtection']['THIRTEEN'] = 	'dreizehn';
+$lang['de_DE']['MathSpamProtection']['FOURTEEN'] = 	'vierzehn';
+$lang['de_DE']['MathSpamProtection']['FIFTEEN'] = 	'fünfzehn';
+$lang['de_DE']['MathSpamProtection']['SIXTEEN'] = 	'sechzehn';
+$lang['de_DE']['MathSpamProtection']['SEVENTEEN'] = 'siebzehn';
+$lang['de_DE']['MathSpamProtection']['EIGHTEEN'] = 	'achtzehn';
+$lang['de_DE']['MathSpamProtection']['WHATIS'] = 'Was ergibt %s plus %s?';
+$lang['de_DE']['MathSpamProtectionField']['SPAMQUESTION'] = 'Spam Schutz Frage: %s';
+$lang['de_DE']['MathSpamProtectionField']['INCORRECTSOLUTION'] = 'Falsche Lösung, noch ein Versuch?;
+
+?>
